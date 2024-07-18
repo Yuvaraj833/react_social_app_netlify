a110b34118d7b337f3dfef5165ae3d0351a39ae8
Yuvaraj <yuvarajdhadha@gmail.com>
2024-07-19 01:10:23 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p style={{marginTop:"1rem"}}> Hi I'm Yuvaraj. I'm Full Stack Developer. This is My First React Application.</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/PostLayout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+const PostLayout = () => {
+  return (
+    <>
+    <Link to="/postPage/1">Post 1</Link>
+    <br />
+    <Link to="/postPage/2">Post 2</Link>
+    <br />
+    <Link to="/postPage/3">Post 3</Link>
+    <br />
+    <Link to="/postPage/newPost">New Post</Link>
+    <Outlet/>
+    </>
+    
+  )
+}
+
+export default PostLayout
